84a1663b80325108c3d23f546b29eb9f9de84051
invailid-expression <rohitsurwade34@gmail.com>
2025-05-17 23:06:58 +0530

    Program.cs Setup

--- BasicMathCalculator/BasicMathCalculator/Program.cs
+++ BasicMathCalculator/BasicMathCalculator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,60 @@ using System.Threading.Tasks;
 
 namespace BasicMathCalculator
 {
-	internal class Program
+	public class Program
 	{
 		static void Main(string[] args)
 		{
+			Console.Write("Hey There! May I know your name?: ");
+			var Name = Console.ReadLine();
+
+			Console.WriteLine("\n");
+
+			var PreviewName = Name.Trim();
+			var Date = DateTime.UtcNow;
+			PreviewName = char.ToUpper(PreviewName[0]) + PreviewName.Substring(1).ToLower().Trim();
+
+			Console.WriteLine("******************************************************************************");
+			Console.Write($"Good Moarnning {PreviewName}, Its {Date} And What You Are Looking For: \n" + 
+				$"\nA. Addition" + 
+				$"\nB. Substraction" + 
+				$"\nC. Multipication" + 
+				$"\nD. Division" + 
+				$"\nE. Percentage" + 
+				$"\nQ. Quit The Programme" + 
+				$"\n\n   Choose The Option: " );
+			char option = Console.ReadLine()[0];
+
+			Console.WriteLine("");
+
+			option = char.ToUpper(option);
+
+			if(option == 'A')
+			{
+				Console.WriteLine("Addition Here");
+			}
+			else if(option == 'B')
+			{
+				Console.WriteLine("Substraction Here");
+			}
+			else if(option == 'C')
+			{
+				Console.WriteLine("Mulltipication Here");
+			}
+			else if(option == 'D')
+			{
+				Console.WriteLine("Division Here");
+			}
+			else if (option == 'E')
+			{
+				Console.WriteLine("Percentage Here");
+			}
+			else if(option == 'Q')
+			{
+				Environment.Exit(0); 
+			}
+			Console.ReadKey();
+
 		}
 	}
 }
